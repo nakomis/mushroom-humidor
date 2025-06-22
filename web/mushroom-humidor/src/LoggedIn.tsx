09cc3f5ebcc55313dc4ab3dcceda03fe45aa989b
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-06-22 22:13:18 +0100

    Added bootstrap for styling

--- web/mushroom-humidor/src/LoggedIn.tsx
+++ web/mushroom-humidor/src/LoggedIn.tsx
@@ -4,8 +4,6 @@ function LoggedIn() {
     if (useAuth().isAuthenticated) {
         window.location.href = `https://mushrooms.sandbox.nakomis.com/`;
         return <div></div>
-    } else {
-        
     }
     return <div></div>
 }
