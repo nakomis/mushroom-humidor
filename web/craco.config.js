587eaa278b6f465ad8ebf62771e5e39447ce92f0
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-06-25 17:06:04 +0100

    Adds craco for path aliases

--- web/craco.config.js
+++ web/craco.config.js
@@ -4,8 +4,10 @@ module.exports = {
     webpack: {
         alias: {
             '@components': path.resolve(__dirname, 'src/components'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
             '@utils': path.resolve(__dirname, 'src/utils'),
             '@images': path.resolve(__dirname, 'src/images'),
+            '@config': path.resolve(__dirname, 'src/config'),
         }
     }
 };
