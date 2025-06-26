7c4b6128a2254021c60dc24bb00e64f24c2a6f95
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-06-26 22:43:08 +0100

    Makes the red of the mushroom easier on the eyes

--- web/deploysandbox.sh
+++ web/deploysandbox.sh
@@ -1,2 +1,3 @@
+scripts/set-config.sh sandbox
 BUCKET_NAME=$(aws s3 ls --profile nakom.is-sandbox| grep mushroomcloudfrontstack-mushroombucket | awk '{ print $3 }')
 npm run build && cd build && aws s3 sync . s3://$BUCKET_NAME --profile nakom.is-sandbox && cd -
