59a7987439d048c9bec195e5c3fdcd1450e539e3
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-06-26 22:43:09 +0100

    Removes duplicate calls to set-config.sh

--- web/scripts/deployprod.sh
+++ web/scripts/deployprod.sh
@@ -1,3 +1,2 @@
-scripts/set-config.sh prod
 BUCKET_NAME=$(aws s3 ls --profile nakom.is-prod | grep mushroomcloudfrontstack-mushroombucket | awk '{ print $3 }')
 npm run build && cd build && aws s3 sync . s3://$BUCKET_NAME --profile nakom.is-prod && cd -
--- web/scripts/deploysandbox.sh
+++ web/scripts/deploysandbox.sh
@@ -1,3 +1,2 @@
-scripts/set-config.sh sandbox
 BUCKET_NAME=$(aws s3 ls --profile nakom.is-sandbox| grep mushroomcloudfrontstack-mushroombucket | awk '{ print $3 }')
 npm run build && cd build && aws s3 sync . s3://$BUCKET_NAME --profile nakom.is-sandbox && cd -
