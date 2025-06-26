7dae55ebedc949de35c941270a0e98e5fab505f6
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-06-26 22:43:06 +0100

    Adds Api gateway, command function and database

---
+++ infra/lib/api-gateway-stack.ts
@@ -0,0 +1,52 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cm from 'aws-cdk-lib/aws-certificatemanager';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import { Construct } from 'constructs';
+
+export interface ApiGatewayStackProps extends cdk.StackProps {
+    rootDomain: string,
+    domainName: string,
+    commandProcessor: lambda.Function
+}
+
+export class ApiGatewayStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: ApiGatewayStackProps) {
+        super(scope, id, props);
+
+        const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZoneLookup', {
+            domainName: props.rootDomain
+        });
+
+        const apiCert = new cm.Certificate(this, "MushroomApiCert", {
+            domainName: props.domainName,
+            validation: cm.CertificateValidation.fromDns(hostedZone)
+        });
+
+        const mushroomGateway = new apigateway.RestApi(this, 'MushroomApiGateway', {
+            restApiName: 'Mushroom API Gateway',
+            description: 'API Gateway for Mushroom application',
+            domainName: {
+                domainName: props.domainName,
+                certificate: apiCert,
+            },
+            deployOptions: {
+                stageName: 'prod',
+                loggingLevel: apigateway.MethodLoggingLevel.INFO,
+            },
+        }); 
+        const commandProcessorIntegration = new apigateway.LambdaIntegration(props.commandProcessor, {
+            proxy: false,
+            integrationResponses: [{
+                statusCode: '202',
+            }],
+        });
+        mushroomGateway.root.addMethod('POST', commandProcessorIntegration, {
+            methodResponses: [{
+                statusCode: '202',
+            }],
+            authorizationType: apigateway.AuthorizationType.IAM
+        });
+    }
+};
