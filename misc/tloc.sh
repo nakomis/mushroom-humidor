e4139469ffcdf038b97a8af9ebb7c2ece9a9bcb4
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-07-04 16:34:42 +0100

    Adds more ignores to tloc

--- misc/tloc.sh
+++ misc/tloc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git ls-files --exclude-standard -- ':!:**/*.[pjs][npv]g' ':!:**/*.ai' ':!:.idea' ':!:**/*.eslintrc' ':!:**/package-lock.json' \
-    ':!:LICENSE' ':!:**/*.drawio' | xargs wc -l
+    ':!:LICENSE' ':!:**/*.bmp' ':!:**/*.drawio' ':!:**/*.fzz' | sed -r 's/\ /\\ /g' | xargs wc -l
 
