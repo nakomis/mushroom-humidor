a49b75f2210b9f2426474df4b8ca5198c2343e2a
Martin Harris <1488244+nakomis@users.noreply.github.com>
2025-07-05 14:40:21 +0100

    Updates Clock, adds clearScreen & draw, tidies drawText

--- embedded/mushroom-humidor/lib/Screen/src/Screen.cpp
+++ embedded/mushroom-humidor/lib/Screen/src/Screen.cpp
@@ -11,26 +11,6 @@
 SSD1306Wire display(SCREEN_ADDRESS, I2C_SDA, I2C_SCL);
 bool initialized = false;
 
-const uint8_t mushroom_ico16x16_bmp[] PROGMEM =
-    {
-        0xF0, 0x0F,
-        0xFC, 0x3E,
-        0xCE, 0x7C,
-        0x8E, 0x7F,
-        0xDF, 0xFF,
-        0xFF, 0x9D,
-        0xF3, 0xB8,
-        0xFB, 0xFD,
-        0xFF, 0xFF,
-        0x20, 0x04,
-        0x20, 0x04,
-        0x20, 0x04,
-        0x20, 0x04,
-        0x20, 0x04,
-        0x60, 0x06,
-        0xC0, 0x03
-    };
-
 Screen::Screen()
 {
 }
@@ -45,16 +25,49 @@ int Screen::loop()
     return 0;
 }
 
+void Screen::drawText(const char *text, int x, int y, uint8_t size, OLEDDISPLAY_TEXT_ALIGNMENT alignment)
+{
+    if (size == 24)
+    {
+        display.setFont(ArialMT_Plain_24);
+    }
+    else if (size == 16)
+    {
+        display.setFont(ArialMT_Plain_16);
+    }
+    else
+    {
+        display.setFont(ArialMT_Plain_10);
+    }
+    display.setTextAlignment(alignment);
+    if (alignment == TEXT_ALIGN_CENTER)
+    {
+        x = SCREEN_WIDTH / 2;
+    }
+    else if (alignment == TEXT_ALIGN_CENTER_BOTH)
+    {
+        x = SCREEN_WIDTH / 2;
+        y = SCREEN_HEIGHT / 2;
+    }
+    display.drawString(x, y, text);
+}
+
 void Screen::drawText(const char *text, int x, int y, uint8_t size)
 {
-    display.setFont(ArialMT_Plain_24);
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.drawString(x, y, text);
-    display.display();
+    drawText(text, x, y, size, TEXT_ALIGN_LEFT);
 }
 
 void Screen::drawImage(const uint8_t *image, int x, int y, int width, int height)
 {
     display.drawXbm(x, y, width, height, image);
+}
+
+void Screen::clearScreen()
+{
+    display.clear();
+}
+
+void Screen::draw()
+{
     display.display();
 }
